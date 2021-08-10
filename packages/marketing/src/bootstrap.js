097d793693ef76fbb9834f4ae16fde1ba453732c
Jose L Mortola <jlmortola@me.com>
2021-08-10 16:29:28 +0200

    feat(container, marketing): add multi-tier navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { createBrowserHistory, createMemoryHistory } from 'history'
 import App from './App'
 
-export const mount = (element) => {
-  if (element) ReactDOM.render(<App />, element)
+export const mount = (element, { defaultHistory, onNavigate }) => {
+  const history = defaultHistory || createMemoryHistory()
+
+  if (onNavigate) history.listen(onNavigate)
+  
+  if (element) ReactDOM.render(<App history={history} />, element)
+
+  return ({
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      if (history.location.pathname !== nextPathname) history.push(nextPathname)
+    }
+  })
 }
 
-if (process.env.NODE_ENV === 'development') mount(document.getElementById('marketing-dev'))
+if (process.env.NODE_ENV === 'development') mount(document.getElementById('marketing-dev'), { defaultHistory: createBrowserHistory() })
